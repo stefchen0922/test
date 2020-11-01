f8e83000d8a47a4d12ea47fcd067389f2d6a322d
Stef Chen <stefsecroad@gmail.com>
2020-11-01 18:47:37 +0800

    Change guess.py

--- guess.py
+++ guess.py
@@ -5,14 +5,21 @@
 
 import random
 
-ans = random.randint(1, 100)
+start = input("請決定隨機數字範圍開始值：")
+end = input("請決定隨機數字範圍結束值：")
+
+ans = random.randint(int(start), int(end))
+count = 0
 while True:
+	count += 1
 	guess = input("請輸入數字: ")
 	if int(guess) != ans:
 		if int(guess) > ans:
 			print("比答案大")
 		elif int(guess) < ans:
 			print("比答案小")
+		print("這是你猜的第", count, "次")
 	else:
 		print("終於猜對了!")
+		print("這是你猜的第", count, "次")
 		break
